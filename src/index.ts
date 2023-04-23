a4ad2f7c742788bb3e71c5566bbaa8baf067382f
Peter Stromquist <peter@twistedstream.com>
2023-04-22 23:42:08 -0500

    feat: support local dev tls cert (in code)

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,32 @@
 // NOTE: The config import should always run first to load the environment
-import { port } from "./utils/config";
+import { port, environment } from "./utils/config";
 import "express-async-errors";
 
 import http from "http";
+import https from "https";
+import fs from "fs";
+import path from "path";
 
 import app from "./app";
 import { logger } from "./utils/logger";
 
-const server = http.createServer(app);
+let serverName: string, server;
+if (environment === "production") {
+  // Docker: create simple HTTP server
+  serverName = "HTTP";
+  server = http.createServer(app);
+} else {
+  // local: create HTTPS server with self-signed cert
+  serverName = "HTTPS";
+  const certOptions = {
+    key: fs.readFileSync(path.resolve("./cert/dev.key")),
+    cert: fs.readFileSync(path.resolve("./cert/dev.crt")),
+  };
+  server = https.createServer(certOptions, app);
+}
 
 server.listen(port, () => {
-  logger.info(`Server is running at http://localhost:${port}`);
+  logger.info(`${serverName} server, is listening on port ${port}`);
 });
 
 export default server;
